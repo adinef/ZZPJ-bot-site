d24696bd5858c9b1803d9ff1559f897dec1c3e2d
Adrian <210171@edu.p.lodz.pl>
2019-04-28 18:05:08 +0200

    165656607 Standard project structure

---
+++ src/main/java/pl/lodz/p/it/zzpj/botsite/BotSiteApplication.java
@@ -0,0 +1,13 @@
+package pl.lodz.p.it.zzpj.botsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotSiteApplication.class, args);
+    }
+
+}
