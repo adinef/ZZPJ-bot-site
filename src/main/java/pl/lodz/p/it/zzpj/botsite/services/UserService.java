daa59a6b64be91cd4c4e385bf27507a400c9a6f2
Dominik Lange <203924@edu.p.lodz.pl>
2019-05-11 18:01:24 +0200

    Refactored UserService

--- src/main/java/pl/lodz/p/it/zzpj/botsite/services/UserService.java
+++ src/main/java/pl/lodz/p/it/zzpj/botsite/services/UserService.java
@@ -1,8 +1,10 @@
 package pl.lodz.p.it.zzpj.botsite.services;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
 import pl.lodz.p.it.zzpj.botsite.entities.User;
 import pl.lodz.p.it.zzpj.botsite.exceptions.UserRetrievalException;
 
-public interface UserService {
+public interface UserService extends UserDetailsService {
     User findByLogin(String login) throws UserRetrievalException;
+    void addUser(User user);
 }
