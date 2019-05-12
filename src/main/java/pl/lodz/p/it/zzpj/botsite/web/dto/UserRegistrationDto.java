3891db8853456b17af2b6eb7d5af42d400879666
Dominik Lange <203924@edu.p.lodz.pl>
2019-05-12 20:21:41 +0200

    165825219 - Created endpoint for registration  + Created UserRegistrationDto  + Created UserControllerTest  + Created UserController

---
+++ src/main/java/pl/lodz/p/it/zzpj/botsite/web/dto/UserRegistrationDto.java
@@ -0,0 +1,16 @@
+package pl.lodz.p.it.zzpj.botsite.web.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserRegistrationDto {
+    String login;
+    String password;
+    String email;
+}
