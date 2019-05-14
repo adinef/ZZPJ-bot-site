e91f37f5ba4bf2f0beba457b4b2424e3e2d0e62e
Kacper Prądzyński <37307285+kacperpradzynski@users.noreply.github.com>
2019-05-15 00:10:58 +0200

    Added Bot Services with Tests

--- src/main/java/pl/lodz/p/it/zzpj/botsite/services/BotServiceImpl.java
+++ src/main/java/pl/lodz/p/it/zzpj/botsite/services/BotServiceImpl.java
@@ -1,10 +1,74 @@
 package pl.lodz.p.it.zzpj.botsite.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pl.lodz.p.it.zzpj.botsite.entities.Bot;
+import pl.lodz.p.it.zzpj.botsite.exceptions.*;
+import pl.lodz.p.it.zzpj.botsite.repositories.BotRepository;
+
+import java.util.Optional;
 
 @Service("mongoBotService")
-public class BotServiceImpl {
+public class BotServiceImpl implements BotService {
+    private final BotRepository botRepository;
 
-    
+    @Autowired
+    public BotServiceImpl(BotRepository botRepository) {
+        this.botRepository = botRepository;
+    }
 
+    public Bot findById(String id) throws BotRetrievalException {
+        try {
+            Optional<Bot> bot = this.botRepository.findById(id);
+            return bot.orElseThrow(() -> new BotNotFoundException("Bot with that ID not found."));
+        } catch (final Exception e) {
+            throw new BotRetrievalException("Could not retrieve bot by ID", e);
+        }
+    }
+
+    public void addBot(Bot bot) throws BotAlreadyExistsException {
+        if (!this.botRepository.findById(bot.getId()).isPresent()) {
+            this.botRepository.save(bot);
+        } else {
+            throw new BotAlreadyExistsException("Bot with this ID is already taken");
+        }
+    }
+
+    public void updateBotName(String id, String name) throws BotNotFoundException {
+        try {
+            Optional<Bot> bot = this.botRepository.findById(id);
+            (bot.orElseThrow(() -> new BotNotFoundException("Bot with that ID not found."))).setName(name);
+            this.botRepository.save(bot.get());
+        } catch (final Exception e) {
+            throw new BotNotFoundException("Bot with that ID not found.", e);
+        }
+    }
+
+    public void updateBotToken(String id, String token) throws BotNotFoundException {
+        try {
+            Optional<Bot> bot = this.botRepository.findById(id);
+            (bot.orElseThrow(() -> new BotNotFoundException("Bot with that ID not found."))).setToken(token);
+            this.botRepository.save(bot.get());
+        } catch (final Exception e) {
+            throw new BotNotFoundException("Bot with that ID not found.", e);
+        }
+    }
+
+    public void updateBotChannel(String id, String channel) throws BotNotFoundException {
+        try {
+            Optional<Bot> bot = this.botRepository.findById(id);
+            (bot.orElseThrow(() -> new BotNotFoundException("Bot with that ID not found."))).setChannel(channel);
+            this.botRepository.save(bot.get());
+        } catch (final Exception e) {
+            throw new BotNotFoundException("Bot with that ID not found.", e);
+        }
+    }
+
+    public void deleteBot(String id) throws BotNotFoundException, BotRetrievalException {
+        if (!this.botRepository.findById(id).isPresent()) {
+            throw new BotNotFoundException("Bot with that ID not found.");
+        } else {
+            this.botRepository.delete(findById(id));
+        }
+    }
 }
