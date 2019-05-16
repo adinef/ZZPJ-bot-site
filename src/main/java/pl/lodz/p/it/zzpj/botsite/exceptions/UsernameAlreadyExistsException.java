c90e5464862c0d436a2457e75873fd278fedb9c2
Kacper Prądzyński <37307285+kacperpradzynski@users.noreply.github.com>
2019-05-17 00:21:53 +0200

    Added new Exceptions constructors

--- src/main/java/pl/lodz/p/it/zzpj/botsite/exceptions/UsernameAlreadyExistsException.java
+++ src/main/java/pl/lodz/p/it/zzpj/botsite/exceptions/UsernameAlreadyExistsException.java
@@ -1,9 +1,17 @@
 package pl.lodz.p.it.zzpj.botsite.exceptions;
 
 public class UsernameAlreadyExistsException extends Exception {
-
+    
     public UsernameAlreadyExistsException(String message) {
         super(message);
+    }
+
+    public UsernameAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UsernameAlreadyExistsException(Throwable cause) {
+        super(cause);
     }
 
 }
