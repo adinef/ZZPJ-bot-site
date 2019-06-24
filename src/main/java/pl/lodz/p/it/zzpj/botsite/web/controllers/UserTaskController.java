894016caea4e0c9b4451b85ba565bef89d29e8f2
Kacper Prądzyński <37307285+kacperpradzynski@users.noreply.github.com>
2019-06-24 17:44:59 +0200

    Added getTaskByID endpoint

--- src/main/java/pl/lodz/p/it/zzpj/botsite/web/controllers/UserTaskController.java
+++ src/main/java/pl/lodz/p/it/zzpj/botsite/web/controllers/UserTaskController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import pl.lodz.p.it.zzpj.botsite.entities.UserTask;
 import pl.lodz.p.it.zzpj.botsite.exceptions.entity.notfound.UserNotFoundException;
 import pl.lodz.p.it.zzpj.botsite.exceptions.entity.notfound.UserTaskNotFoundException;
+import pl.lodz.p.it.zzpj.botsite.exceptions.entity.retrieval.UserTaskRetrievalException;
 import pl.lodz.p.it.zzpj.botsite.exceptions.entity.saving.UserTaskAdditionException;
 import pl.lodz.p.it.zzpj.botsite.exceptions.entity.saving.UserTaskUpdateException;
 import pl.lodz.p.it.zzpj.botsite.exceptions.entity.unconsistent.UserTaskIdAlreadyExistsException;
@@ -46,6 +47,20 @@ public class UserTaskController {
         return userTaskUserDTOS;
     }
 
+    //SECURITY + GET SINGLE TASK
+    @Secured("ROLE_USER")
+    @GetMapping(
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public UserTaskUserDTO getTaskByUserId(@PathVariable("id") final Long taskId)
+            throws UserTaskRetrievalException {
+        UserTaskUserDTO userTaskUserDTOS = new UserTaskUserDTO();
+        UserTask userTask = userTaskService.findById(taskId);
+        modelMapper.map(userTask, userTaskUserDTOS);
+        return userTaskUserDTOS;
+    }
+
     // SECURITY
     @Secured("ROLE_USER")
     @PostMapping(
@@ -60,7 +75,7 @@ public class UserTaskController {
 
     // CHECK IF USER HAS RIGHT TO UPDATE THE TASK
     @Secured("ROLE_USER")
-    @PutMapping(
+            @PutMapping(
             value = "edit/{id}",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
