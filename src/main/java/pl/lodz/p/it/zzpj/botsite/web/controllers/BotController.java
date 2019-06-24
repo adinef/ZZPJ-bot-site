58556910083bf4d8c8fb118c3a7dc9874ced0778
Adrian Fijałkowski <fijaladrian@gmail.com>
2019-06-24 21:21:31 +0200

    166671992 Bot controllers CRUD

--- src/main/java/pl/lodz/p/it/zzpj/botsite/web/controllers/BotController.java
+++ src/main/java/pl/lodz/p/it/zzpj/botsite/web/controllers/BotController.java
@@ -5,20 +5,26 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pl.lodz.p.it.zzpj.botsite.config.security.PrincipalProvider;
 import pl.lodz.p.it.zzpj.botsite.entities.Bot;
+import pl.lodz.p.it.zzpj.botsite.entities.User;
+import pl.lodz.p.it.zzpj.botsite.exceptions.entity.deletion.BotDeletionException;
+import pl.lodz.p.it.zzpj.botsite.exceptions.entity.notfound.BotNotFoundException;
 import pl.lodz.p.it.zzpj.botsite.exceptions.entity.retrieval.BotRetrievalException;
 import pl.lodz.p.it.zzpj.botsite.exceptions.entity.retrieval.UserRetrievalException;
 import pl.lodz.p.it.zzpj.botsite.exceptions.entity.saving.BotAdditionException;
+import pl.lodz.p.it.zzpj.botsite.exceptions.entity.saving.BotUpdateException;
 import pl.lodz.p.it.zzpj.botsite.exceptions.entity.unconsistent.BotAlreadyExistsException;
 import pl.lodz.p.it.zzpj.botsite.services.BotService;
 import pl.lodz.p.it.zzpj.botsite.services.UserService;
+import pl.lodz.p.it.zzpj.botsite.web.dto.MyUserDetails;
 import pl.lodz.p.it.zzpj.botsite.web.dto.bots.BotCreationDTO;
+import pl.lodz.p.it.zzpj.botsite.web.dto.bots.BotEditDTO;
 import pl.lodz.p.it.zzpj.botsite.web.dto.bots.BotViewDTO;
 
+import javax.print.attribute.standard.Media;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -42,7 +48,7 @@ public class BotController {
 
     @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping(
-            value = "",
+            value = "user",
             consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE
     )
@@ -55,19 +61,53 @@ public class BotController {
     }
 
     @PreAuthorize("hasRole('ROLE_USER')")
-    @GetMapping (
-            value = "",
+    @GetMapping(
+            value = "user",
             produces = MediaType.APPLICATION_JSON_VALUE
     )
     @ResponseStatus(HttpStatus.OK)
-    public List<BotViewDTO> getAllBotsForCurrentUser() throws BotRetrievalException {
-        List<Bot> bots = this.botService.findAllForUserId(principalProvider.getUserId());
+    public List<BotViewDTO> getAllBotsForUser() throws BotRetrievalException, UserRetrievalException {
+        User user = this.userService.findByLogin(principalProvider.getName());
+        List<Bot> bots = this.botService.findAllForUserId(user.getId());
         return mapList(bots);
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @PutMapping(
+            value = "user/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public BotViewDTO editBot(@PathVariable("id") Long id, @RequestBody BotEditDTO dto)
+            throws BotNotFoundException, BotRetrievalException, BotUpdateException {
+        Bot botById = this.botService.findById(id);
+        if (!botById.getUser().getLogin().equals(principalProvider.getName())) {
+            throw new BotUpdateException("Can't update somebody else's bot.");
+        }
+        this.botService.updateBotChannel(botById.getId(), dto.getChannel());
+        Bot updated = this.botService.updateBotName(botById.getId(), dto.getName());
+        return this.modelMapper.map(updated, BotViewDTO.class);
+    }
+
+    @PreAuthorize("hasRole('ROLE_USER')")
+    @DeleteMapping(
+            value = "user/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteBot(@PathVariable("id") Long id)
+            throws BotRetrievalException, BotDeletionException, BotNotFoundException {
+        Bot botById = this.botService.findById(id);
+        if (!botById.getUser().getLogin().equals(principalProvider.getName())) {
+            throw new BotDeletionException("Can't delete somebody's else bot.");
+        }
+        this.botService.deleteBot(botById.getId());
     }
 
     private List<BotViewDTO> mapList(List<Bot> bots) {
         List<BotViewDTO> dtoList = new ArrayList<>();
-        bots.forEach( b -> this.modelMapper.map(b, BotViewDTO.class));
+        bots.forEach( b -> dtoList.add(this.modelMapper.map(b, BotViewDTO.class)));
         return dtoList;
     }
 }
